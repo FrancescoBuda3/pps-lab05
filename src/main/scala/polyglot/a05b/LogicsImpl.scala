4daae9288848c46f2091aee6e2444e0b985314bf
Francesco Buda <francesco.buda3@studio.unibo.it>
2025-04-02 14:33:05 +0200

    feat: finish exercise 2

--- src/main/scala/polyglot/a05b/LogicsImpl.scala
+++ src/main/scala/polyglot/a05b/LogicsImpl.scala
@@ -1,11 +1,26 @@
 package polyglot.a05b
 
 import polyglot.a05b.Logics
+import scala.util.Random
 
 /** solution and descriptions at https://bitbucket.org/mviroli/oop2019-esami/src/master/a05b/sol2/ */
 class LogicsImpl(private val size: Int) extends Logics:
-  override def tick(): Unit = {}
+  private var tickCount: Int = 0
+  private val random: Random = Random()
+  private val initial: (Int, Int) = (random.nextInt(size-2)+1,random.nextInt(size-2)+1)
 
-  override def isOver: Boolean = false
+  override def tick(): Unit =
+    tickCount = tickCount + 1
 
-  override def hasElement(x: Int, y: Int): Boolean = false
+  override def isOver: Boolean = initial match
+    case (ix, iy) if iy - tickCount < 0 || iy + tickCount >= size => true
+    case (ix, iy) if ix - tickCount < 0 || ix + tickCount >= size => true
+    case _ => false
+
+  override def hasElement(x: Int, y: Int): Boolean = initial match
+    case (ix,iy) if x == ix && Math.abs(y - iy) <= tickCount => true
+    case (ix, iy) if y == iy && Math.abs(x - ix) <= tickCount => true
+    case (ix, iy) if x - y == ix - iy && Math.abs(x - ix) <= tickCount => true
+    case (ix, iy) if x + y == ix + iy && Math.abs(x -ix) <= tickCount => true
+    case _ => false
+
